1f3a1051ae88804919ac7a2b00074eb1957573cc
VladislavPopovM <55542677+VladislavPopovM@users.noreply.github.com>
2020-11-18 12:07:14 +0700

    Update local_settings.py

--- simpl_reg/local_settings.py
+++ simpl_reg/local_settings.py
@@ -2,8 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-SECRET_KEY = '6(rzqzvsz!6zzaigu9%tiqj*x-ym8^$3cs+z89hb0-0hsyux&o'
-
 DEBUG = True
 
 DATABASES = {
@@ -12,7 +10,3 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '567306842155-aufnact8cp02qfrtejs766m3p4ubgb9p.apps.googleusercontent.com' # Google Consumer Key
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'OuzCRAWBweaJneakqho2eAa2' # Google Consumer Secret
